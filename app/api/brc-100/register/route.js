e20f69e967380812809fa2a2568cf173d02ecf5e
Deggen <d.kellenschwiler@bsvassociation.org>
2025-07-31 13:39:31 -0500

    add wallet

--- app/api/brc-100/register/route.js
+++ app/api/brc-100/register/route.js
@@ -1,17 +1,28 @@
+import { Signature, BigNumber, Utils, Hash, ECDSA, PublicKey } from "@bsv/sdk"
+
 export async function POST(req) {
     try {
         const body = await req.json()
         const { alias, identityKey, protocolID, keyID, signature } = body
 
+        const msg = new BigNumber(Hash.sha256(Utils.toArray(alias, 'utf8')))
+        const sig = Signature.fromHex(signature)
+        const pubKey = PublicKey.fromHex(identityKey)
+
         // check that the signature is valid for the identityKey
+        const isValid = ECDSA.verify(msg, sig, pubKey)
+        if (!isValid) return Response.json({ error: 'Invalid signature' }, { status: 400 })      
+    
 
         // check if alias is available
+        const existingRecord = await req.db.collection('aliases').findOne({ alias })
+        if (existingRecord) return Response.json({ error: 'Alias already exists' }, { status: 400 })
 
         // if yes then create a record mapping the alias to the identityKey and return success
+        const success = await req.db.collection('aliases').insertOne({ alias, identityKey, signature })
+        if (!success) return Response.json({ error: 'Failed to register alias' }, { status: 400 })
 
-        // if no return error
-
-        return Response.json(response, { status: 200 })
+        return Response.json({ [alias]: identityKey }, { status: 200 })
     } catch (error) {
         console.log({ error })
         return Response.json({ error: error.message }, { status: 400 })
---
+++ app/wallet.js
@@ -0,0 +1,11 @@
+export async function makeWallet(chain, storageURL, privateKey) {
+    const keyDeriver = new KeyDeriver(new PrivateKey(privateKey, 'hex'));
+    const storageManager = new WalletStorageManager(keyDeriver.identityKey);
+    const signer = new WalletSigner(chain, keyDeriver, storageManager);
+    const services = new Services(chain);
+    const wallet = new Wallet(signer, services);
+    const client = new StorageClient(wallet, storageURL);
+    await client.makeAvailable();
+    await storageManager.addWalletStorageProvider(client);
+    return wallet;
+}
